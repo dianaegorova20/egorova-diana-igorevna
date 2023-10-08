0d9746e1f8bdcad483db6029b2ad72e77a692c6f
dianaegorova20 <146988823+dianaegorova20@users.noreply.github.com>
2023-10-08 17:05:28 +0300

    Add files via upload

---
+++ Лабораторная работа 1, Егорова Д.И/Статистика посещений сайта.py
@@ -0,0 +1,6 @@
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+
+kolvo = len(users)
+unik = len(set(users))
+slovar = {"Общее количество": kolvo, "Уникальные посещения": unik}
+print(slovar)
